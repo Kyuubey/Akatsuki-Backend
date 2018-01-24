37c036c66c11b8ed1053887504566ba3d88f23f1
yuwui <me@yui.pet>
2018-01-24 19:58:59 +0100

    fix needsmorejpeg

--- routes/api/needsmorejpeg.py
+++ routes/api/needsmorejpeg.py
@@ -4,6 +4,11 @@ from io import BytesIO
 async def handle(req):
     io = BytesIO()
 
-    Image.open(BytesIO(req.files[''].body)).save(io, 'JPEG', quality=25)
+    im = Image.open(BytesIO(req.files[''].body))
+    r,g,b,a = im.split()
 
-    return req.Response(body=io.getvalue(), mime_type='image/png', encoding='UTF-8')
+    rgb_im = Image.merge("RGB", (r,g,b))
+
+    rgb_im.save(io, 'JPEG', quality=1)
+
+    return req.Response(body=io.getvalue(), mime_type='image/jpeg', encoding='UTF-8')
