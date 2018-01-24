be9ff392cd18dd04af4305ed903f949068fde009
yuwui <me@yui.pet>
2018-01-24 19:25:32 +0100

    add mirror endpoint

---
+++ routes/api/mirror.py
@@ -0,0 +1,10 @@
+from PIL import Image, ImageOps
+from io import BytesIO
+
+async def handle(req):
+    im = Image.open(BytesIO(req.files[''].body))
+
+    io = BytesIO()
+    ImageOps.mirror(im).save(io, format='PNG')
+
+    return req.Response(body=io.getvalue(), mime_type='image/png', encoding='UTF-8')
