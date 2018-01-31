3096b11bf5b1dddee3778cd482692ed0db968bbc
yuwui <me@yui.pet>
2018-01-31 16:59:27 +0100

    auto scale text on ilikethat endpoint

--- routes/api/ilikethat.py
+++ routes/api/ilikethat.py
@@ -15,19 +15,22 @@ def handle(req):
 
     txt = req.query['text']
 
+    w, h = font.getsize(txt)
+
+    font_size = int(125 / w * font.size)
+
+    font = ImageFont.truetype(f'{path}/public/fonts/comicsans.ttf', min(font_size, font.size))
+
     txt_img = Image.new('RGBA', (125, 60), (0, 0, 0, 0))
     txt_draw = ImageDraw.Draw(txt_img)
 
-    lines = textwrap.wrap(txt, width=125)
-
     im = Image.open(f'{path}/public/img/ilikethat.png')
 
-    txt_draw.multiline_text(
-        (0, 0), "\n".join(lines), fill='black', font=font, anchor='center')
+    txt_draw.text((0,0), txt, fill='black', font=font, anchor='center')
 
-    im2 = txt_img.rotate(19)
+    im2 = txt_img.rotate(19, expand=1)
 
-    im.paste(im2, (190, 160), im2)
+    im.paste(im2, (175, 175), im2)
 
     io = BytesIO()
     im.save(io, format='PNG')
